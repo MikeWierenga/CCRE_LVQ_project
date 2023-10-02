a4958b057877fbdfc43e1f53e879b5342d75d234
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-10-02 16:38:58 +0200

    added integrate function to cre. checked for normal distribution per patient in datanotebook. updated main file

---
+++ Calculating_distance_measures/CCRE_distance/cre.py
@@ -0,0 +1,78 @@
+from scipy.integrate import quad
+import numpy as np
+import scipy.stats as ss
+import math
+from sklearn.preprocessing import MinMaxScaler
+from scipy import special
+from scipy import integrate
+
+class CRE:
+    def __init__(self, data):
+        self.data = data.iloc[0, 2:]
+        self.nbin = len(self.data)
+
+        self.EqualSizeBinFlag = True 
+    
+    def calculate_edges(self):
+        if self.EqualSizeBinFlag:
+            
+            edges = np.histogram_bin_edges(self.data, self.nbin)
+           
+        else:
+            prc = np.linspace(0,100, self.nbin+1)
+            edges = np.percentile(self.data, prc, interpolation="midpoint")
+        return edges
+    
+    def calculate_cre(self):
+        #step 1 calculate the CDF based on matlabs histcounts function
+        CP, bin = np.histogram(list(self.data), list(self.calculate_edges())) 
+        hist_dist = ss.rv_histogram((CP, bin))
+        CP = hist_dist.cdf(bin)[1:]
+
+        dl = np.diff(bin) / sum(np.diff(bin))
+        # -1 cause we are interested in the values greater or equal than x
+        FC = 1 - CP.T
+ 
+        logFC = []
+        for i in FC:
+            if i == 0 :
+                logFC.append(0)
+            else:
+
+                logFC.append(np.log(i))
+
+        if dl.T.dot(FC) == 0:
+            cre = 0
+        else:
+         
+            cre_part_one = -dl.T.dot(np.multiply(FC, logFC))
+            cre_part_two = dl.T.dot(FC)
+            cre = np.divide(cre_part_one, cre_part_two)
+
+        print(f'cre = {cre}')
+
+    # CRE WITH GAUSSIAN DISTRIBUTION
+    # based on this post https://stats.stackexchange.com/questions/187828/how-are-the-error-function-and-standard-normal-distribution-function-related
+    def cumulative_distribution(self, x, mu, sigma):
+        error_function = 1 - (0.5 * (1 + math.erf( (x-mu) / (sigma*np.sqrt(2)))))
+    
+        if error_function == 0:
+            log_errorfunction= 0 
+        else:
+
+            log_errorfunction = np.log(error_function)
+        return error_function*log_errorfunction
+    
+
+    
+    def cre_gaussian_distribution(self):
+        # data = np.array(self.data).reshape(-1,1)
+        # data = MinMaxScaler(feature_range=(0,1)).fit_transform(data)
+        data = np.array(self.data)
+        sigma = np.sqrt(np.var(data))
+        mu = np.mean(data)
+        cumulative_distribution_results = []
+        for i in data:
+            cumulative_distribution_results.append(self.cumulative_distribution(x=i, sigma=sigma, mu=mu))
+   
+        print((- 1 * integrate.quad(self.cumulative_distribution, 0, np.inf, args=(mu, sigma))[0]))
--- Calculating_distance_measures/main/main.py
+++ Calculating_distance_measures/main/main.py
@@ -4,6 +4,9 @@ import load_data
 sys.path.append('euclidean_distance')
 import euclidean_distance
 import numpy as np
+sys.path.append('CCRE_distance')
+import cre 
+import pandas as pd
 
 class Main():
 
@@ -24,30 +27,46 @@ class Main():
         diagnosis.rename(columns={0: "diagnosis_label"}, inplace=True)
 
         df = data.combine_dataframes(center, diagnosis, feature_vectors)
-        # calculating the Euclidean distance
-        eu_distance = euclidean_distance.Euclidean_Distance(self.hospital, self.diagnosis)
+        
+        #normalizing the data(everything is between 0 and 1)
+        df_labels = df.iloc[:, :2]
+        normalized_df =  (df.iloc[:, 2:]-df.iloc[:, 2:].mean())/df.iloc[:, 2:].std()
+        # normalized_df = (df.iloc[:, 2:]-df.iloc[:, 2:].min())/(df.iloc[:, 2:].max()-df.iloc[:, 2:].min())
+        dataframes = [df_labels, normalized_df]
+        df = pd.concat(dataframes, axis=1)
 
-        # get dataframe that only consists of with the labels mentioned above
-        specific_df = df[(df['center_label'] == eu_distance.hospital) & (df['diagnosis_label'] == eu_distance.diagnosis)]
+        # # calculating the Euclidean distance
+        # eu_distance = euclidean_distance.Euclidean_Distance(self.hospital, self.diagnosis)
 
-        # connect all rows with eachother and make sure there are no duplicates
-        connections = eu_distance.connect_rows(specific_df)
-        distances = []
-        for i in connections:
-            position1 = np.array(specific_df.iloc[i[0]][2:])
-            position2 = np.array(specific_df.iloc[i[1]][2:])
-            distances.append(eu_distance.measure_distance(position1, position2))
+        # # get dataframe that only consists of with the labels mentioned above
+        # specific_df = df[(df['center_label'] == eu_distance.hospital) & (df['diagnosis_label'] == eu_distance.diagnosis)]
 
-        # calculate average distance
-        average_distance = eu_distance.calculate_average(distances)
-        return average_distance
+        # # connect all rows with eachother and make sure there are no duplicates
+        # connections = eu_distance.connect_rows(specific_df)
+        # distances = []
+        # for i in connections:
+        #     position1 = np.array(specific_df.iloc[i[0]][2:])
+        #     position2 = np.array(specific_df.iloc[i[1]][2:])
+        #     distances.append(eu_distance.measure_distance(position1, position2))
+        # print(distances)
+        # # calculate average distance
+        # average_distance = eu_distance.calculate_average(distances)
+        # return average_distance
 
+        # calculate the CRE
+        cre_distance = cre.CRE(df)
+    
+        cre_distance.calculate_cre()
+        cre_distance.cre_gaussian_distribution()
+
+        #calculating CCRE 
+        
 hospitals = ["UMCG", "CUN", "UGOSM"]
 diagnosis = ["HC", "AD", "PD"]
 
-for hospital in hospitals:
-    for diagnosi in diagnosis:
-        if (hospital == "CUN") & (diagnosi == "AD"):
-            continue
-        print(f"{hospital} {diagnosi}: {Main(hospital, diagnosi).main()}")
-
+# for hospital in hospitals:
+#     for diagnosi in diagnosis:
+#         if (hospital == "CUN") & (diagnosi == "AD"):
+#             continue
+#         print(f"{hospital} {diagnosi}: {Main(hospital, diagnosi).main()}")
+Main("UMCG", "AD").main()
