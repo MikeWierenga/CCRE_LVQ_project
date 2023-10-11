251d0d470bfe88658873c4d67753ea3ab939b40c
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-10-11 11:17:54 +0200

    added a new fit function so that the mean cov invcov and detcov are calculated at init

--- Calculating_distance_measures/CCRE_distance/ccre.py
+++ Calculating_distance_measures/CCRE_distance/ccre.py
@@ -4,33 +4,40 @@ import numpy as np
 from scipy import stats
 from scipy.stats.contingency import margins
 class CCRE:
-    def __init__(self, mu, cov, invcov):
-        pass
-    
-    def calculate_covariance_matrix(self, data):
-        return np.cov(data.astype(float), rowvar=False)
-
-    def calculate_joint_dist(self, data):
-        """
-        based on the formula given on this website(05/10/2023): https://www.geeksforgeeks.org/visualizing-the-bivariate-gaussian-distribution-in-python/
-        returns: nx1 joint probability distribution function
-        """
-        # split this function so that we keep the mu cov and invcov
-        cov = self.calculate_covariance_matrix(data)
-        invcov = np.linalg.pinv(cov)
+    def __init__(self, data):
+        self.data = data
+        self.mean = 0
+        self.cov = []
+        self.invcov = []
+        self.detcov = []
         
+        self.fit(data)
+        # print(f"{self.mean} \n {self.cov} \n {self.invcov} \n hi{self.detcov}")
+
+    def fit(self, data):
         x = data[:, 0]
         y = data[:, 1]
-        mu = np.mean(np.array([x,y ]), axis=1)
-        data = data.T
-        long_version = []
-        for i in range(data.shape[1]):
-            first_part = (data[:, i] - mu).reshape(-1,1).T
-            last_part = first_part.T
-            formula =  -(first_part@invcov@last_part)/2
-            final_formula = (1/ (np.sqrt(2*np.pi*np.linalg.det(cov)))) * np.exp(formula.astype(float))
-            long_version.append(float(final_formula))
-        return long_version 
+        self.mean = np.mean([x,y], axis=1)
+        self.cov = np.cov(data)
+        self.invcov = np.linalg.pinv(self.cov)
+        self.detcov = np.linalg.det(self.cov)
+
+
+    def calculate_joint_dist(self, new_entry):
+        """
+        based on the formula given on this website(05/10/2023): https://www.geeksforgeeks.org/visualizing-the-bivariate-gaussian-distribution-in-python/
+        returns: 1x1 joint probability distribution function
+        """
+        # split this function so that we keep the mu cov and invcov
+        print(new_entry)
+        first_part = (new_entry[:] - self.mean).reshape(-1,1).T
+        last_part = first_part.T
+        last_part = first_part.T
+        
+        formula =  -(first_part@self.invcov@last_part)/2
+        final_formula = (1/ (np.sqrt(2*np.pi*self.detcov))) * np.exp(formula.astype(float))[0][0]
+        
+        return final_formula
 
     def calculate_margin_pdf(self, X, mu , sigma, sigma2):
 
