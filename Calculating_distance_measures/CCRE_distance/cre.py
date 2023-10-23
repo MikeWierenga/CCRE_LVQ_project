ea2dd885152bf0f1225cdefc7f8a49676877fdf8
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-10-23 14:31:15 +0200

    code cleaning cre class

--- Calculating_distance_measures/CCRE_distance/cre.py
+++ Calculating_distance_measures/CCRE_distance/cre.py
@@ -1,9 +1,6 @@
 from scipy.integrate import quad
 import numpy as np
 import scipy.stats as ss
-import math
-from sklearn.preprocessing import MinMaxScaler
-from scipy import special
 from scipy import integrate
 
 
@@ -27,22 +24,16 @@ class CRE:
 
         returns: float
         """
-        error_function = ss.norm.sf(x, mu, sigma)
-        # error_function = 1 - (0.5 * (1 + math.erfc((x-mu) / (sigma*np.sqrt(2)))))
-        # error_function = 1 - (0.5 * (1 + math.erf( (x-mu) / (sigma*np.sqrt(2)))))
-        if error_function <0:
-            error_function = 0
+        error_function = 1- ss.norm.cdf(x, mu, sigma)
             
-        if error_function == 0:
+        if error_function <= 0:
             log_errorfunction= 0 
         else:
-
-            log_errorfunction = ss.norm.logsf(x, mu,sigma)
-            # log_errorfunction = np.log(error_function)
+            log_errorfunction = np.log(error_function)
+        
         return error_function*log_errorfunction
     
 
-    
     def cre_gaussian_distribution(self):
         """
         Calculates the cre value for a continuous random variable based on Rao's paper
