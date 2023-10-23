57df122b024014a537e7ba24112842502c9ec8ab
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-10-23 14:45:35 +0200

    added comments

--- Calculating_distance_measures/CCRE_distance/ccre.py
+++ Calculating_distance_measures/CCRE_distance/ccre.py
@@ -1,8 +1,5 @@
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rv_continuous.html#scipy.stats.rv_continuous
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rv_continuous.expect.html#scipy.stats.rv_continuous.expect
 import numpy as np
 from scipy import stats
-from scipy.stats.contingency import margins
 class CCRE:
     def __init__(self, data):
         self.data = data
@@ -41,6 +38,7 @@ class CCRE:
     def mean_conditional_distribution(self):
         """
         Based on formula in book on statistics
+        returns: mean of conditional distribution (float)
         """
        
         mean_y = self.mean[self.position_y]
@@ -61,9 +59,8 @@ class CCRE:
     def cov_conditional_distribution(self):
         """
         Calculating the covariance of the conditional distribution p(Y|X) based on book on statistics
+        returns: covariance of conditional distribution (float)
         """
-        
-        
         cov_yy = self.cov[self.position_y][self.position_y]
         cov_yx = self.cov[self.position_y][self.position_x]
        
@@ -80,30 +77,38 @@ class CCRE:
         return formula
      
 
-    def calculate_expactation_value(self, y, x, mu, sigma, sigma2, cov, cre_class):
+    def calculate_expectation_value(self, y, x, mu, sigma, sigma2, cov, cre_class):
         # expectation value E(X) = integral from -inf to inf of x * probability of x
         # our case E(cre(Y|X)) = cre(Y|X) * pX(x)
+        """
+        Calculates the expectation value of cre(Y|X)
+        """
         self.data = x
         conditional_mean = self.mean_conditional_distribution()
-        cre = cre_class.cumulative_distribution(y, conditional_mean, cov) #this will be the function to calculate the cre
+        cre = cre_class.cumulative_distribution(y, conditional_mean, cov)
         
-        p = self.calculate_margin_pdf(x, mu, sigma, sigma2) # this will be the pdf function
-        # test = np.e**(-(x-mu)**2/2*sigma2)
-        # # print(test)
+        p = self.calculate_margin_pdf(x, mu, sigma, sigma2) 
+        
         formula = cre * p
         return formula 
     
 
-    # FOR TESTING 
-    def calculate_expectation_value_xy(self, x, y, mu_y, sigma_y, sigma2_y, cov, cre_class):
+    
+    def calculate_expectation_value_xy(self, y, x, mu_y, sigma_y, sigma2_y, cov, cre_class):
+        """
+        Calculates the expectation value of cre(X|Y)
+        """
         self.data = y
         conditional_mean = self.mean_conditional_distribution()
-        cre = cre_class.cumulative_distribution(x, conditional_mean, cov) #this will be the function to calculate the cre
-        p = self.calculate_margin_pdf(y, mu_y, sigma_y, sigma2_y) # this will be the pdf function
+        cre = cre_class.cumulative_distribution(x, conditional_mean, cov) 
+        p = self.calculate_margin_pdf(y, mu_y, sigma_y, sigma2_y) 
         
         formula = cre * p
         return formula 
       
-    def calculate_CCRE(self, entropy_X, expectation_value_YX):
-        ccre = entropy_X - expectation_value_YX
+    def calculate_CCRE(self, entropy, expectation_value):
+        """
+        Calculates the CCRE
+        """
+        ccre = (entropy - expectation_value) / entropy
         return ccre
