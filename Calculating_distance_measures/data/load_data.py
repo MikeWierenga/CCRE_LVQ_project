38e912d1b2066a6b937be37fa6d2707d90c5f559
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-10-27 17:39:25 +0200

    added function that combines a part of the dataframe based on amount specified

--- Calculating_distance_measures/data/load_data.py
+++ Calculating_distance_measures/data/load_data.py
@@ -1,29 +1,37 @@
 import yaml
 import pandas as pd
+import os
 class Load_Data:
     def __init__(self):
-       self.config = self.get_config()
-
+        self.config = self.get_config()
+    
     def get_config(self):
-        with open("main/config.yaml", "r") as stream:
+        with open("Calculating_distance_measures/main/config.yaml", "r") as stream:
             config = yaml.safe_load(stream)
         return config
     
     def combine_dataframes(self, *dataframes):
-        # print(dataframes)
         return pd.concat(dataframes, axis=1)
 
     def get_dataframe(self, filename):
         df = pd.read_csv(self.config[filename], header = None)
         return df
 
-test = Load_Data()
-feature_vectors = test.get_dataframe('feature_vectors')
-feature_vectors = feature_vectors.set_axis(list(range(1, 36)), axis=1)
-center = test.get_dataframe('center_label')
-center.rename(columns={0: "center_label"}, inplace=True)
+    def limit_dataframe(self, df, amount):
+        list_of_df = []
+        for center in df.center_label.unique():
+            for diagnosis in df.diagnosis_label.unique():
+                list_of_df.append(df[(df.center_label == center) & (df.diagnosis_label == diagnosis)][:amount] )
+            
+        return pd.concat(list_of_df)
+# test = Load_Data()
+# feature_vectors = test.get_dataframe('feature_vectors')
+# feature_vectors = feature_vectors.set_axis(list(range(1, 36)), axis=1)
+# center = test.get_dataframe('center_label')
+# center.rename(columns={0: "center_label"}, inplace=True)
 
-diagnosis = test.get_dataframe('diagnosis_label')
-diagnosis.rename(columns={0: "diagnosis_label"}, inplace=True)
+# diagnosis = test.get_dataframe('diagnosis_label')
+# diagnosis.rename(columns={0: "diagnosis_label"}, inplace=True)
 
-test.combine_dataframes(center, diagnosis, feature_vectors)
+# df = test.combine_dataframes(center, diagnosis, feature_vectors)
+# print(test.limit_dataframe(df, 10))
