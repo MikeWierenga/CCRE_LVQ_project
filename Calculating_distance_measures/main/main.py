c98a0c1b437f01ab9c97aaffcf9bdcf88b52cdbc
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-11-01 10:54:44 +0100

    fixed main according to ccre changes

--- Calculating_distance_measures/main/main.py
+++ Calculating_distance_measures/main/main.py
@@ -44,7 +44,7 @@ class Main():
         df = data.combine_dataframes(center, diagnosis, feature_vectors)
         df = df[(df['center_label'].isin(self.hospital)) & (df['diagnosis_label'].isin(self.diagnosis))]
         
-        df = data.limit_dataframe(df, 5)
+        df = data.limit_dataframe(df, 10)
         
         # create csv file
         if not os.path.isfile(f"CCRE_distances_{self.hospital}_{self.diagnosis}.csv"):
@@ -66,7 +66,7 @@ class Main():
         # connect rows
         connection = neighbours.neighbours(df)
         for i in connection.connect_neighbours():
-            
+            print(i)
             x = np.array(df.iloc[i[0], 2:-1]).astype(float).reshape(-1,1)
             y = np.array(df.iloc[i[1], 2:-1]).astype(float).reshape(-1,1)
             
@@ -84,13 +84,12 @@ class Main():
             #calculate expecation value X|Y
             mean_y = np.mean(y)
             sigma_y = np.std(y)
-            sigma2_y = np.var(y)
 
             new_data= np.concatenate((x, y), axis = 1)
 
             ccre_distance = ccre.CCRE(new_data.T)
             cov_conditional_dist = ccre_distance.cov_conditional_distribution() 
-            expect_value_cre_xy = integrate.dblquad(ccre_distance.calculate_expectation_value_xy, -np.inf, np.inf, 0, np.inf, args=(mean_y, sigma_y, sigma2_y, cov_conditional_dist, cre_x))
+            expect_value_cre_xy = integrate.dblquad(ccre_distance.calculate_expectation_value_xy, -np.inf, np.inf, 0, np.inf, args=(mean_y, sigma_y, cov_conditional_dist))
    
             ccre_value = (cre_x_value + (expect_value_cre_xy[0]))/cre_x_value
             if ccre_value < 0:
@@ -99,8 +98,12 @@ class Main():
             
             
             # write to csv files
-            ccre_data = [df.index[i[0]], df.index[i[1]],self.hospital, self.diagnosis, cre_x_value, cre_y_value, expect_value_cre_xy[0], ccre_value]
-            euclidean_data = [df.index[i[0]], df.index[i[1]],self.hospital, self.diagnosis, euclidean_value, euclidean_similarity_value]
+            center_label_x = df.iloc[i[0]]['center_label']
+            center_label_y = df.iloc[i[1]]['center_label']
+            diagnosis_label_x = df.iloc[i[0]]['diagnosis_label']
+            diagnosis_label_y = df.iloc[i[1]]['diagnosis_label']
+            ccre_data = [df.index[i[0]], df.index[i[1]], f'{center_label_x}_{center_label_y}', f'{diagnosis_label_x}_{diagnosis_label_y}', cre_x_value, cre_y_value, expect_value_cre_xy[0], ccre_value]
+            euclidean_data = [df.index[i[0]], df.index[i[1]], f'{center_label_x}_{center_label_y}', f'{diagnosis_label_x}_{diagnosis_label_y}', euclidean_value, euclidean_similarity_value]
             if self.created_cre_file:
                 with open("CCRE_distances.csv", "a", newline="") as file:
                     writer = csv.writer(file)
@@ -127,7 +130,5 @@ diagnosis = ["HC", "AD", "PD"]
 #             continue
 #         Main(hospital, diagnosi).main()
 
-Main(hospitals, ["AD", "PD"]).main()
-Main(hospitals, ["HC", "AD"]).main()
-Main(hospitals, ["HC", "PD"]).main()
-Main(hospitals, ["HC", "AD", "PD"]).main()
+Main(hospitals, diagnosis).main()
+
