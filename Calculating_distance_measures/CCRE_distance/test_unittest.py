266ae3c6419e5b4fb4dca7a6d554cdc4821c33eb
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-11-01 10:56:34 +0100

    removed unwanted code

--- Calculating_distance_measures/CCRE_distance/test_unittest.py
+++ Calculating_distance_measures/CCRE_distance/test_unittest.py
@@ -80,61 +80,11 @@ class Test_testcre(unittest.TestCase):
         for i in np.linspace(0.1, 10, 10):
 
             dataset = np.random.normal(0,i,1000)
-            # cre_class = cre.CRE(dataset)
-            # cre_value = cre_class.cre_gaussian_distribution()
             cre_value = self.calculate_cre(dataset)
             self.assertGreater(cre_value, before)
             
             before = cre_value
 
-    
-    # def test_conditional_cre(self):
-    #     y = np.random.normal(0,1,10).reshape(-1,1)
-    #     x = np.random.normal(0,1, 10).reshape(-1,1)
-    #     mean_x = np.mean(x)
-    #     sigma = np.std(x)
-    #     sigma2 = np.var(x)
-
-    #     original_data = np.concatenate((y, x), axis = 1)
-       
-    #     # print(f'cre(x) = {cre_x_value} cre(y) = {cre_y_value}')
-    #     ccre_class = ccre.CCRE(original_data.T)
-    #     cov_conditional_dist = ccre_class.cov_conditional_distribution() 
-    #     # print(f'cre(Y|X = 0){integrate.quad(ccre_class.calculate_expactation_value, 0, np.inf, args = (0, mean_x, sigma, sigma2, cov_conditional_dist, cre_class))}')
-        
-    #     for i in np.linspace(0.01, 1, 100):
-    #         print(f'PERCENTAGE = {i}')
-    #         print(np.pi/2*i)
-    #         m = np.array([[1,0], [np.sin(np.pi/2*i),np.cos(np.pi/2*i)]]).T
-    
-    #         new_data = np.dot(original_data, m)
-    #         new_x = new_data[:, 0].reshape(-1,1)
-         
-            
-    #         mean_x = np.mean(new_x)
-    #         sigma = np.std(new_x)
-    #         sigma2 = np.var(new_x)
-
-    #         new_y = new_data[:, 1].reshape(-1,1)
-    #         new_data= np.concatenate((new_y, new_x), axis = 1)
-    #         # print(new_data[0])
-    #         cre_class = cre.CRE(new_x)
-    #         cre_y = cre.CRE(new_y)
-    #         cre_value = cre_class.cre_gaussian_distribution()
-    #         cre_y_value = cre_y.cre_gaussian_distribution()
-    #         print(f'cre(x) = {cre_value}, cre(y) = {cre_y_value}')
-
-    #         ccre_class = ccre.CCRE(new_data.T)
-    #         cov_conditional_dist = ccre_class.cov_conditional_distribution() 
-    #         print(cov_conditional_dist)
-    #         cov = np.cov(new_data.T)
-    #         print(np.linalg.det(cov))
-    #         print(cov)
-            
-    #         print(np.linalg.pinv(cov))
-    #         print(f'cre(Y|X = 0){integrate.quad(ccre_class.calculate_expactation_value, 0, np.inf, args = (0, mean_x, sigma, sigma2, cov_conditional_dist, cre_class))} \n')
-        
-       
     def test_unrelated_to_related_gaussian_distribution(self):
 
         x = np.random.normal(0,1, 100).reshape(-1,1)
