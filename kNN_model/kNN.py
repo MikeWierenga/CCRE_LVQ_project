66a1cad836984abc7e03a8e60f7ad65e2aaeca19
MikeWierenga <mk.wierenga@st.hanze.nl>
2023-11-09 13:34:21 +0100

    added gridsearch

--- kNN_model/kNN.py
+++ kNN_model/kNN.py
@@ -1,4 +1,5 @@
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 import numpy as np
 import scipy.stats as ss
 from scipy import integrate
@@ -7,20 +8,34 @@ sys.path.append('/home/fcourse1/Desktop/afstudeerstage/code/CCRE_LVQ_project/Cal
 import cre 
 import ccre
 class KNN:
-    def __init__(self, neighbours, metric):
-        self.neighbours = neighbours
-        self.metric = metric
-        self.x = []
-        self.y = []
-    
+    def __init__(self):
+        self.neighbours = 5
+        self.weights = 'uniform'
+        self.algorithm = 'auto'
+        self.leaf_size = 30
+        self.p = 2
+        self.metric = 'minkowski'
+        self.n_jobs = -1
+        self.model = self.create_model()
+     
     def create_model(self):
-        model = KNeighborsClassifier(n_neighbors=self.neighbours, metric=self.metric, n_jobs=-1)
+        model = KNeighborsClassifier()
         return model
     
-    def fit_model(self, x, y):
-        self.x = x
-        self.y = y
+    def set_parameters(self, n_neighbors =5, weights='uniform', algorithm='auto', leaf_size=30, p =2, metric='minkowski', n_jobs=-1):
+         self.neighbours = n_neighbors
+         self.weights = weights
+         self.algorithm = algorithm
+         self.leaf_size = leaf_size
+         self.p = p
+         self.metric = metric
+         self.n_jobs = n_jobs
 
+    def gridsearch(self):
+        params = [self.neighbours, self.weights, self.algorithm, self.leaf_size, self.p, self.metric, self.n_jobs]
+        model = GridSearchCV(self.model, param_grid=params, n_jobs=-1)
+        return model
+    
     def make_predict(self, model, new_entry):
         return model.predict(new_entry)
     
